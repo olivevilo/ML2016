a88c11cc5037a43f1273e81898762141da49f9c1
Oliveck10 <oliveck10@gmail.com>
2016-09-23 12:53:10 +0800

    hw0_1:first version(list handling)

--- hw0/hw0_1.py
+++ hw0/hw0_1.py
@@ -1,9 +1,28 @@
-import pandas as pd
 import sys
 
-col = sys.argv[1]
+col = int(sys.argv[1])
 fileName = sys.argv[2]
 
-df = pd.read_table(fileName)
+with open(fileName) as f:
+    contents = f.read().splitlines()
 
-print df
+results = []
+
+for content, i in zip(contents, range(len(contents))):
+    df = content.split(" ")  # .remove(" ")
+    # eliminate blank element
+    for e in df:
+        if(e == ''):
+            df.remove(e)
+    # from string to numeric style
+    try:
+        df[col] = int(df[col])
+    except ValueError:
+        df[col] = float(df[col])
+
+    results.append(df[col])
+
+results.sort()
+# final output format
+results = map(str, results)
+print ','.join(results)
